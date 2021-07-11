985c12193f044214abc9c37848df0eeb55e2c206
Bert Hertogen <bert@hertogen.net>
2021-07-11 19:01:17 +0100

    Add mongo

--- listener/listener.py
+++ listener/listener.py
@@ -9,20 +9,25 @@
 An example to show receiving events from an Event Hub.
 """
 import os
+import json
 from azure.eventhub import EventHubConsumerClient
 from dotenv import load_dotenv
 from datetime import datetime
+from pymongo import MongoClient
+from pprint import pprint
 
 load_dotenv()
 
 EVENTHUB_CONNECTION_STRING = os.environ["EVENTHUB_CONNECTION_STRING"]
 CONSUMER_GROUP_NAME = os.environ['CONSUMER_GROUP_NAME']
+MONGODB_CONNECTION_STRING = os.environ['MONGODB_CONNECTION_STRING']
 
 
 def on_event(partition_context, event):
     # Put your code here.
     # If the operation is i/o intensive, multi-thread will have better performance.
     print("Received event from partition: {} {}.".format(partition_context.partition_id, event.enqueued_time))
+    write_event(event)
 
 def on_partition_initialize(partition_context):
     # Put your code here.
@@ -47,11 +52,29 @@ def on_error(partition_context, error):
     else:
         print("An exception: {} occurred during the load balance process.".format(error))
 
+def write_event(event):
+    print('Type: ', type(event))
+    print("Connecting ...")
+    eventJson={
+        **event.body_as_json(encoding='UTF-8'),
+        'enqueued_time':event.enqueued_time,
+        'offset':event.offset,
+        'sequence_number':event.sequence_number,
+    }
+    result=db.events.insert_one(eventJson)
+    print('Created {0}'.format(result.inserted_id))
+
 if __name__ == '__main__':
     consumer_client = EventHubConsumerClient.from_connection_string(
         conn_str=EVENTHUB_CONNECTION_STRING,
         consumer_group=CONSUMER_GROUP_NAME
     )
+
+    print("Checking db connection...")
+    client = MongoClient(MONGODB_CONNECTION_STRING)
+    db=client.mxchip
+    serverStatusResult=db.command("serverStatus")
+    print('Connection made to ', serverStatusResult['host'])
 
     try:
         with consumer_client:

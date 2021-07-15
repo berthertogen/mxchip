0532c4a57ff75e1f61470867dfacbc2bc80f955f
Bert Hertogen <bert@hertogen.net>
2021-07-15 19:35:36 +0100

    Add app

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -1,24 +1,41 @@
 const path = require('path');
- const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { webpack } = require('webpack');
 
- module.exports = {
+let apiHost;
+var setupConfig = () => {
+  switch (process.env.NODE_ENV) {
+    case 'production':
+      apiHost = 'http://hertogen.synology.me:8081';
+      break;
+    default:
+      apiHost = 'http://localhost:3000';
+      break;
+  }
+};
+setupConfig();
+
+module.exports = {
   mode: 'development',
-   entry: {
-     index: './src/index.js',
-   },
-   devtool: 'inline-source-map',
-   devServer: {
-     contentBase: './dist',
-   },
-   plugins: [
-     new HtmlWebpackPlugin({
+  entry: {
+    index: './src/index.js',
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
       title: 'Development',
       template: './src/index.html',
-     }),
-   ],
-   output: {
-     filename: '[name].bundle.js',
-     path: path.resolve(__dirname, 'dist'),
-     clean: true,
-   },
- };
+    }),
+    new webpack.DefinePlugin({
+      __API__: apiHost
+    })
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+};
